1704fdf6bd1234b53cd034d7f106ad9ea6ec8dc0
fanghuiuo <fanghuiuo@163.com>
2019-10-15 12:43:25 +0800

    2.0

--- scrapyxiaozhu/scrapyxiaozhu/spiders/xiaozhu.py
+++ scrapyxiaozhu/scrapyxiaozhu/spiders/xiaozhu.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
-from scrapyxiaozhu.items import ScrapyxiaozhuItem
+from  scrapyxiaozhu.items import ScrapyxiaozhuItem
 
 class XiaozhuSpider(scrapy.Spider):
     name = 'xiaozhu'
@@ -11,7 +11,7 @@ class XiaozhuSpider(scrapy.Spider):
         listurls=response.xpath('//div[@id="page_list"]/ul/li/a/@href').getall()
         for url in listurls:
             fzitem=ScrapyxiaozhuItem()
-            fzitem['fzmc']=response.xpath('//div[@class="result_intro"]/span/text()').get()
+            
             yield scrapy.Request(url=url,callback=self.parse_detail, meta={"fzitem":fzitem})
         
         
@@ -27,8 +27,9 @@ class XiaozhuSpider(scrapy.Spider):
             yield scrapy.Request(next_url, callback=self.parse)    
     
     def parse_detail(self,response):
-        fzitem = response.meta["fzitem"]       
-        fzitem['fzdz']=response.xpath('//div[@class="con_1"]/div/p/@title').get()
+        fzitem = response.meta["fzitem"] 
+        fzitem['fzmc']=response.xpath('//div[@class="pho_info"]/h4/em/text()').get()
+        fzitem['fzdz']=response.xpath('//div[@class="pho_info"]/p/@title').get()
         fzitem['fzjg']=response.xpath('//div[@id="pricePart"]/div/span/text()').get()
         fzitem['fzxm']=response.xpath('//div[@class="w_240"]/h6/a/@title').get()
         print(fzitem)
