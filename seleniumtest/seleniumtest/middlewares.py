b7c249e6943e876d3b950536066773e725fe3dd6
fanghuiuo <fanghuiuo@163.com>
2019-11-07 14:48:58 +0800

    3.0

--- seleniumtest/seleniumtest/middlewares.py
+++ seleniumtest/seleniumtest/middlewares.py
@@ -12,20 +12,21 @@ from selenium.webdriver.chrome.options import Options
 import time
 
 class airMiddleware(object):
+    
     def process_request(self,request,spider):
-        chrome_options = Options()
-        chrome_options.add_argument('--headless')  # 使用无头谷歌浏览器模式
-        chrome_options.add_argument('--disable-gpu')#谷歌文档提到需要加上这个属性来规避bug
-        chrome_options.add_argument('--no-sandbox')#解决DevToolsActivePort文件不存在的报错
+        chrome_options=Options()
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--disable-gpu')
+        chrome_options.add_argument('--no-sandbox')
         #chrome_options.add_argument('--hide-scrollbars')#隐藏滚动条, 应对一些特殊页面
-        # 指定谷歌浏览器路径
-        self.driver = webdriver.Chrome(chrome_options=chrome_options)
-        if request.url != 'https://www.aqistudy.cn/historydata/':#不是第一页
-            self.driver.get(request.url)
-            time.sleep(1)
-            print(self.driver.title)
-            html = self.driver.page_source
-            self.driver.quit()
-            return scrapy.http.HtmlResponse(url=request.url, body=html.encode('utf-8'), encoding='utf-8',request=request)
+        self.driver=webdriver.Chrome(chrome_options=chrome_options)
+        self.driver.get(request.url)
+        time.sleep(1)
+        print(self.driver.title)
+        html=self.driver.page_source
+        self.driver.quit()
+        return scrapy.http.HtmlResponse(url=request.url,body=html.encode('utf-8'),encoding='utf-8',request=request)
+
+
 
     
