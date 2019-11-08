a3e7812faaf94da95a6ed62c2fd87734118343a4
fanghuiuo <fanghuiuo@163.com>
2019-11-08 13:06:12 +0800

    3.0

--- seleniumtest/seleniumtest/pipelines.py
+++ seleniumtest/seleniumtest/pipelines.py
@@ -12,14 +12,14 @@ from seleniumtest.items import rqItem
 
 class SeleniumtestPipeline(object):
     def __init__(self):
-        self.conn = pymysql.connect(host='127.0.0.1', port=3306, user='root', password='fh@791204', db='xiaozhu')
-        self.strsql='insert into fz (fzmc,fzdz,fzjg,fzxm) values (%s,%s,%s,%s)'
+        self.conn = pymysql.connect(host='127.0.0.1', port=3306, user='root', password='fh@791204', db='air')
+        self.yfsql='insert into airmonth (yf,cs,aqi,aqifw,zldj,) values (%s,%s,%s,%s)'
     def open_spider(self,spider):
         self.cursor=self.conn.cursor()
 
     def process_item(self,item,spider):
         if isinstance(item,yfItem):
-            self.cursor.execute(self.strsql,(item['fzmc'],item['fzdz'],item['fzjg'],item['fzxm']))
+            self.cursor.execute(self.yfsql,(item['fzmc'],item['fzdz'],item['fzjg'],item['fzxm']))
             self.conn.commit()
             print(item)
             return item
