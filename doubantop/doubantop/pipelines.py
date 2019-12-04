609b95acfa59b78d07cbb3ac976a3c65f698ff5b
fanghuiuo <fanghuiuo@163.com>
2019-12-04 21:27:37 +0800

    3.0

--- doubantop/doubantop/pipelines.py
+++ doubantop/doubantop/pipelines.py
@@ -4,8 +4,23 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
+import pymysql
 
 class DoubantopPipeline(object):
+    def __init__(self):
+        self.conn = pymysql.connect(host='127.0.0.1', port=3306, user='root', password='fh@791204', db='movietop')
+        self.strsql='insert into movie (dymz,dydy,dybj,dyzy,dylx,dygj,dyyy,dysyrq,dypc,dyym,dyjj) values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'
+    def open_spider(self, spider):
+        self.cursor=self.conn.cursor()
+        
+    
     def process_item(self, item, spider):
+        self.cursor.execute(self.strsql,(item['dymz'],item['dydy'],item['dybj'],item['dyzy'],item['dylx'],item['dygj'],item['dyyy'],item['dysyrq'],item['dypc'],item['dyym'],item['dyjj']))
+        self.conn.commit()
+        print(item)
         return item
+
+    def close_spider(self,spider):
+        self.cursor.close()
+        self.conn.close()
+    
--- doubantop/doubantop/settings.py
+++ doubantop/doubantop/settings.py
@@ -19,30 +19,31 @@ NEWSPIDER_MODULE = 'doubantop.spiders'
 #USER_AGENT = 'doubantop (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 32
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 1
 # The download delay setting will honor only one of:
-#CONCURRENT_REQUESTS_PER_DOMAIN = 16
-#CONCURRENT_REQUESTS_PER_IP = 16
+CONCURRENT_REQUESTS_PER_DOMAIN = 16
+CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
-#COOKIES_ENABLED = False
+COOKIES_ENABLED = False
 
 # Disable Telnet Console (enabled by default)
-#TELNETCONSOLE_ENABLED = False
+TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
-#   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-#   'Accept-Language': 'en',
-#}
+DEFAULT_REQUEST_HEADERS = {
+   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+   'Accept-Language': 'en',
+   'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36',
+}
 
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
@@ -52,9 +53,9 @@ ROBOTSTXT_OBEY = True
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    'doubantop.middlewares.DoubantopDownloaderMiddleware': 543,
-#}
+DOWNLOADER_MIDDLEWARES = {
+    'doubantop.middlewares.doubanMiddleware': 543,
+}
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
@@ -64,9 +65,9 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'doubantop.pipelines.DoubantopPipeline': 300,
-#}
+ITEM_PIPELINES = {
+    'doubantop.pipelines.DoubantopPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
