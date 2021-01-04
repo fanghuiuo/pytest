bb0566be54540fdf6d8d927aca8608e4eb7651e8
fanghuiuo <fanghuiuo@163.com>
2021-01-04 08:48:34 +0800

    new

--- pandastest/moivepandas.py
+++ pandastest/moivepandas.py
@@ -3,7 +3,7 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 def mysqltoexcel():
-    eg=create_engine('mysql+pymysql://root:%s@1227.0.0.1:3306/pytest' % ('root888'))
+    eg=create_engine('mysql+pymysql://root:root888@1227.0.0.1:3306/pytest')
     strsql='select * from dbmoive'
     rows=pd.read_sql_query(strsql,eg)
     df=pd.DataFrame.from_records(rows)
