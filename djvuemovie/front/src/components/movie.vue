d9e5a39a9a5a8ba2589ae825e858adb36312bff3
fanghuiuo <fanghuiuo@163.com>
2021-02-19 23:19:08 +0800

    new

--- djvuemovie/front/src/components/movie.vue
+++ djvuemovie/front/src/components/movie.vue
@@ -9,30 +9,24 @@
           <el-menu-item-group>
             <template slot="title">分组一</template>
             <el-menu-item index="1-1">选项1</el-menu-item>
-            <el-menu-item index="1-2">选项2</el-menu-item>
+
           </el-menu-item-group>
           <el-menu-item-group title="分组2">
             <el-menu-item index="1-3">选项3</el-menu-item>
           </el-menu-item-group>
-          <el-submenu index="1-4">
-            <template slot="title">选项4</template>
-            <el-menu-item index="1-4-1">选项4-1</el-menu-item>
-          </el-submenu>
+
         </el-submenu>
         <el-submenu index="2">
           <template slot="title"><i class="el-icon-menu"></i>导航二</template>
           <el-menu-item-group>
             <template slot="title">分组一</template>
             <el-menu-item index="2-1">选项1</el-menu-item>
-            <el-menu-item index="2-2">选项2</el-menu-item>
+
           </el-menu-item-group>
           <el-menu-item-group title="分组2">
             <el-menu-item index="2-3">选项3</el-menu-item>
           </el-menu-item-group>
-          <el-submenu index="2-4">
-            <template slot="title">选项4</template>
-            <el-menu-item index="2-4-1">选项4-1</el-menu-item>
-          </el-submenu>
+
         </el-submenu>
         <el-submenu index="3">
           <template slot="title"
@@ -41,15 +35,12 @@
           <el-menu-item-group>
             <template slot="title">分组一</template>
             <el-menu-item index="3-1">选项1</el-menu-item>
-            <el-menu-item index="3-2">选项2</el-menu-item>
+
           </el-menu-item-group>
           <el-menu-item-group title="分组2">
             <el-menu-item index="3-3">选项3</el-menu-item>
           </el-menu-item-group>
-          <el-submenu index="3-4">
-            <template slot="title">选项4</template>
-            <el-menu-item index="3-4-1">选项4-1</el-menu-item>
-          </el-submenu>
+
         </el-submenu>
       </el-menu>
     </el-aside>
@@ -68,13 +59,22 @@
       </el-header>
 
       <el-main>
-        <el-table :data="info" >
+        <el-table :data="info.slice((currentPage-1)*pagesize,currentPage*pagesize)" >
           <el-table-column prop="dym" label="电影名" width="140">
           </el-table-column>
           <el-table-column prop="dy" label="导演" width="120">
           </el-table-column>
           <el-table-column prop="bj" label="编剧"> </el-table-column>
         </el-table>
+        <el-pagination
+            @size-change="handleSizeChange"
+            @current-change="handleCurrentChange"
+            :current-page="currentPage"
+            :page-sizes="[5, 10, 20, 40]"
+            :page-size="pagesize"
+            layout="total, sizes, prev, pager, next, jumper"
+            :total="info.length">    //这是显示总共有多少数据，
+       </el-pagination>
       </el-main>
     </el-container>
   </el-container>
@@ -84,12 +84,27 @@
 export default {
   data() {
     return {
+      currentPage:1, //初始页
+      pagesize:10,    //    每页的数据
+      totle: 0,
       info: [],
     }
   },
   mounted() {
-    this.$axios.get("http://127.0.0.1:8000/api/1").then((response) => (this.info.push(response.data)))
-  }
+
+    this.$axios.get("http://127.0.0.1:8000/api/").then((response) => (this.info=response.data))
+  },
+   methods: {
+        // 初始页currentPage、初始每页数据数pagesize和数据data
+        handleSizeChange: function (size) {
+                this.pagesize = size;
+                console.log(this.pagesize)  //每页下拉显示数据
+        },
+        handleCurrentChange: function(currentPage){
+                this.currentPage = currentPage;
+                console.log(this.currentPage)  //点击第几页
+        },
+      }
 }
 </script>
 
